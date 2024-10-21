58f5843a6c9171a5acbc263d8faaec0c0b9a0272
Leandro Freitas <leandro.freitas@cadmus.com.br>
2024-10-21 14:22:58 -0300

    feat: ajustes na forma de utilização

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# download-gallery
+# Download Gallery
+
+#### 1. Fazer download e instalação do [Python](https://www.python.org/downloads/)
+#### 2. Rodar o comando no terminal `pip install -r requirements.txt`
+#### 3. Rodar o comando no terminal `python scrapping.py`
+#### 4. Após rodar esse comando vai aparecer no terminal a opção de adicionar o link do deviant
+#### 5. Após o link ser adicionado o download começará a ser feito no diretório que vai aparecer no próprio terminal e vai ser dividido por nome de usuário e galeria, por exemplo "leandro/dragonball"
+![](exemplo_utilizacao.gif)
--- scrapping.py
+++ scrapping.py
@@ -14,16 +14,27 @@ def download_deviantart_gallery(url, output_dir):
         url
     ]
 
-    # Executa o comando e baixa a galeria
     try:
+        print("Download está iniciando, aguarde a conclusão...")
         result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print("Download concluído!")
     except subprocess.CalledProcessError as e:
         print(f"Erro ao baixar a galeria: {e.stderr.decode()}")
 
-# URL da galeria DeviantArt e diretório de destino
-gallery_url = ""
-output_directory = ""
+def __get_path_name(url):
+    # removendo o deviantart
+    url_splitted = url.split('.com/')
+    # quebrando o resto da url nas barras
+    url_splitted = url_splitted[1].split('/')
+    # retornar o path completo
+    return f"{url_splitted[0]}/{url_splitted[-1]}"
 
-# Chama a função para baixar a galeria
-download_deviantart_gallery(gallery_url, output_directory)
+if __name__ == "__main__":
+    continue_scrapping = True
+    while continue_scrapping:
+        gallery_url = input("Digite a URL da galeria DeviantArt: ")
+        output_directory = f"./galerias/{__get_path_name(gallery_url)}"
+        print(output_directory)
+        download_deviantart_gallery(gallery_url, output_directory)
+
+        continue_scrapping = input("Deseja continuar o scrapping? (s/n): ").lower() == "s"
